4c0f25c4a152d4fc4149b877cd8a51d7e3a1a690
adrienlauer <adrien.lauer@gmail.com>
2013-06-11 00:01:24 +0200

    Added a type clashing binding to test that spring beans are effectively only binded with a name

--- nuun-spring-plugin/src/test/java/org/nuunframework/spring/SpringPluginTest.java
+++ nuun-spring-plugin/src/test/java/org/nuunframework/spring/SpringPluginTest.java
@@ -16,6 +16,8 @@
  */
 package org.nuunframework.spring;
 
+import com.google.inject.AbstractModule;
+import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.name.Names;
 import org.junit.AfterClass;
@@ -25,6 +27,7 @@ import org.nuunframework.kernel.Kernel;
 import org.nuunframework.spring.sample.AbstractService2;
 import org.nuunframework.spring.sample.Service1;
 import org.nuunframework.spring.sample.Service1Internal;
+import org.nuunframework.spring.sample.Service1InternalBis;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.StaticApplicationContext;
 
@@ -33,6 +36,7 @@ import static org.fest.assertions.Assertions.assertThat;
 public class SpringPluginTest {
 
     static Kernel kernel;
+    static Injector injector;
 
 
     @BeforeClass
@@ -46,6 +50,13 @@ public class SpringPluginTest {
         kernel.init();
         kernel.start();
 
+        // add a second guice-only implementation of Service1
+        injector = kernel.getMainInjector().createChildInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                bind(Service1.class).to(Service1InternalBis.class);
+            }
+        });
     }
 
     @AfterClass
@@ -55,66 +66,38 @@ public class SpringPluginTest {
 
     @Test
     public void spring_bean_should_be_injectable_by_type_and_name() {
-        Service1 service1_by_name = kernel.getMainInjector().getInstance(Key.get(Service1Internal.class, Names.named("service1")));
+        Service1 service1_by_name = injector.getInstance(Key.get(Service1Internal.class, Names.named("service1")));
         assertThat(service1_by_name).isNotNull();
     }
 
     @Test
     public void spring_bean_should_be_injectable_by_interface_and_name() {
-        Service1 service1 = kernel.getMainInjector().getInstance(Key.get(Service1.class, Names.named("service1")));
+        Service1 service1 = injector.getInstance(Key.get(Service1.class, Names.named("service1")));
         assertThat(service1).isNotNull();
     }
 
     @Test
     public void spring_bean_should_be_injectable_by_parent_type_and_name() {
-        AbstractService2 service2 = kernel.getMainInjector().getInstance(Key.get(AbstractService2.class, Names.named("service2")));
+        AbstractService2 service2 = injector.getInstance(Key.get(AbstractService2.class, Names.named("service2")));
         assertThat(service2).isNotNull();
     }
-
-//    @Test
-//    public void spring_bean_should_be_injectable_by_type() {
-//        assertThat(service1.serve()).isEqualTo(Service1Internal.class.getName());
-//        // by default spring
-//        assertThat(service1).isEqualTo(kernel.getMainInjector().getInstance(Service1.class));
-//        // with guice this should not work but both bindings are backed by the same applicationcontext
-//        assertThat(service1).isEqualTo(service1_by_name);
-//
-//        AbstractService2 service2 = kernel.getMainInjector().getInstance(AbstractService2.class);
-//        assertThat(service2).isNotNull();
-//        assertThat(service2.serve2()).isNotNull();
-//        assertThat(service2.serve2()).isEqualTo(Service2Internal.class.getName());
-//        assertThat(service2).isEqualTo(kernel.getMainInjector().getInstance(AbstractService2.class));
-//
-//        Service3 service3 = kernel.getMainInjector().getInstance(Service3.class);
-//        assertThat(service3).isNotNull();
-//        assertThat(service3.serve()).isEqualTo(Service3Internal.class.getName());
-//
-////      kernel.getMainInjector().injectMembers(instance);
-//    }
 
     @Test
     public void canHandle_should_return_true_for_application_context_classes() {
         InternalDependencyInjectionProvider provider = new InternalDependencyInjectionProvider();
-
         ApplicationContext application = new StaticApplicationContext();
-
         assertThat(provider.canHandle(application.getClass())).isTrue();
-
     }
 
     @Test
     public void canHandle_should_return_true_for_bean_factory_classes() {
         InternalDependencyInjectionProvider provider = new InternalDependencyInjectionProvider();
-
         ApplicationContext application = new StaticApplicationContext();
-
         assertThat(provider.canHandle(application.getAutowireCapableBeanFactory().getClass())).isTrue();
-
     }
 
     @AfterClass
     public static void clear() {
         kernel.stop();
     }
-
 }
